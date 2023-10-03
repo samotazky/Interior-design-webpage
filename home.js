37c301aded0cdba126e19087ab09104b8b596a48
samotazky <samotazky82@gmail.com>
2023-10-03 16:16:00 +0200

    home-css: complete

--- home.js
+++ home.js
@@ -10,16 +10,18 @@ document.querySelector(".menu-btn").onclick = function () {
   body.classList.toggle("active-overlay")
 }
 
+// prerobiť tak aby sa pozadia menili postupne (použiť opacity)
+// vložiť do backgroundImg viac pozadí kt sa budú meniť
 
-let homeBody = document.querySelector("body")
-let backgroundImg = ["url('/img/home.png')", "url('/img/Projekty.png')"]
-let currentIndex = 0
+// let homeBody = document.querySelector("body")
+// let backgroundImg = ["url('/img/home.png')", "url('/img/Projekty.png')"]
+// let currentIndex = 0
 
-let changeBackground = () => {
+// let changeBackground = () => {
 
-  homeBody.style.backgroundImage = backgroundImg[currentIndex]
+//   homeBody.style.backgroundImage = backgroundImg[currentIndex]
   
-  currentIndex = (currentIndex + 1)% backgroundImg.length
-}
+//   currentIndex = (currentIndex + 1)% backgroundImg.length
+// }
   
-  let interval = setInterval(changeBackground, 5000)
+//   let interval = setInterval(changeBackground, 5000)
