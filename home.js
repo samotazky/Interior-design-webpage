b52b2abde960c9e0045bfd0f7ebcafca25209a42
samotazky <samotazky82@gmail.com>
2023-10-03 15:33:56 +0200

    first commit

---
+++ home.js
@@ -0,0 +1,25 @@
+//OVERLAY
+let overlay = document.querySelector("#overlay")
+let menuBtn = document.querySelector(".menu-btn")
+let btn1 = document.querySelector(".bar-1")
+let body = document.querySelector("body")
+
+document.querySelector(".menu-btn").onclick = function () {
+  overlay.classList.toggle("is-active")
+  menuBtn.classList.toggle("change")
+  body.classList.toggle("active-overlay")
+}
+
+
+let homeBody = document.querySelector("body")
+let backgroundImg = ["url('/img/home.png')", "url('/img/Projekty.png')"]
+let currentIndex = 0
+
+let changeBackground = () => {
+
+  homeBody.style.backgroundImage = backgroundImg[currentIndex]
+  
+  currentIndex = (currentIndex + 1)% backgroundImg.length
+}
+  
+  let interval = setInterval(changeBackground, 5000)
