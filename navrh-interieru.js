2b8d812b093fe93738fe12d09251f87d96e29c9c
samotazky <samotazky82@gmail.com>
2023-10-23 22:32:48 +0200

    navrh-interieru: css, imgs, all: +footer

---
+++ navrh-interieru.js
@@ -0,0 +1,11 @@
+//OVERLAY
+let overlay = document.querySelector("#overlay")
+let menuBtn = document.querySelector(".menu-btn")
+let btn1 = document.querySelector(".bar-1")
+let body = document.querySelector("body")
+
+document.querySelector(".menu-btn").onclick = function () {
+  overlay.classList.toggle("is-active")
+  menuBtn.classList.toggle("change")
+  body.classList.toggle("active-overlay")
+}
