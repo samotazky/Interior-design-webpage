a952ae7295fe372f61967ab99779edd17c987dc1
samotazky <samotazky82@gmail.com>
2023-11-11 23:00:06 +0100

    text

--- o-nas.js
+++ o-nas.js
@@ -1,16 +1,6 @@
-//OVERLAY
-let overlay = document.querySelector("#overlay")
-let menuBtn = document.querySelector(".menu-btn")
-let btn1 = document.querySelector(".bar-1")
-let body = document.querySelector("body")
 
-document.querySelector(".menu-btn").onclick = function () {
-  overlay.classList.toggle("is-active")
-  menuBtn.classList.toggle("change")
-  body.classList.toggle("active-overlay")
-}
 window.addEventListener("DOMContentLoaded",() => {
 
   let url = window.location.hash
-  console.log(url);
+  console.log("Toto je url" + url);
 })
